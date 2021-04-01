361a93e1643564fdd6ca955b0b9d655b8f9e3689
operezcham <operezcham@gmail.com>
2021-03-31 18:24:48 -0600

    PLL clock

--- bram_comm_2/bram_comm_2.runs/impl_1/runme.sh
+++ bram_comm_2/bram_comm_2.runs/impl_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/dell_/bram_comm_2/bram_comm_2.runs/impl_1'
+HD_PWD='C:/Users/dell_/Documents/vivado_zncc/bram_comm_2/bram_comm_2.runs/impl_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log soc_wrapper.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source soc_wrapper.tcl -notrace
 
 
--- bram_comm_2/bram_comm_2.sdk/FSBL/Debug/src/image_mover.d
+++ bram_comm_2/bram_comm_2.sdk/FSBL/Debug/src/image_mover.d
@@ -15,7 +15,7 @@ src/image_mover.o src/image_mover.o: ../src/image_mover.c ../src/fsbl.h \
  ../../FSBL_bsp/ps7_cortexa9_0/include/xil_io.h \
  ../../FSBL_bsp/ps7_cortexa9_0/include/xstatus.h \
  ../../FSBL_bsp/ps7_cortexa9_0/include/xil_assert.h ../src/fsbl_debug.h \
- C:\Users\dell_\bram_comm_2\bram_comm_2.sdk\soc_wrapper_hw_platform_0/ps7_init.h \
+ C:\Users\dell_\Documents\vivado_zncc\bram_comm_2\bram_comm_2.sdk\soc_wrapper_hw_platform_0/ps7_init.h \
  ../src/image_mover.h \
  ../../FSBL_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
  ../src/fsbl_hooks.h ../src/md5.h \
@@ -59,7 +59,7 @@ src/image_mover.o src/image_mover.o: ../src/image_mover.c ../src/fsbl.h \
 
 ../src/fsbl_debug.h:
 
-C:\Users\dell_\bram_comm_2\bram_comm_2.sdk\soc_wrapper_hw_platform_0/ps7_init.h:
+C:\Users\dell_\Documents\vivado_zncc\bram_comm_2\bram_comm_2.sdk\soc_wrapper_hw_platform_0/ps7_init.h:
 
 ../src/image_mover.h:
 
